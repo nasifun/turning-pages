c02b5c390cec5c3a0c7119deee52fdd8e27cd207
nasifun <nasifun@gmail.com>
2025-06-23 02:18:51 +0200

    server connected

--- server.js
+++ server.js
@@ -18,10 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('✅ MongoDB connected'))
 .catch(err => console.log('MongoDB connection error:', err));
 
